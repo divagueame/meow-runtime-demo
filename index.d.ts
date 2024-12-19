b1b30a1b1319e6163371e0aef1cf92b2bc078de6
Martin Arce <m.arce@joviva.de>
2024-12-19 15:02:23 +0100

    Add examples

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,24 @@
+// This works. and type of 'meow' is inferred properly.
+// runtimeConfig: {
+//   public: {
+//     meow: process.env.meow === "cucu" ? "cucu" : "not a cucu",
+//   },
+// },
 declare module "@nuxt/schema" {
   interface PublicRuntimeConfig {
     meow: "cucu" | "not a cucu";
   }
 }
 export {};
+
+// Suggested approach on docs does not type public.meow
+// It works only if I delete ./server/tsconfig.json
+// https://nuxt.com/docs/guide/going-further/runtime-config
+//
+// declare module "nuxt/schema" {
+//   interface PublicRuntimeConfig {
+//     meow: "cucu" | "not a cucu";
+//   }
+// }
+// export {};
+//
